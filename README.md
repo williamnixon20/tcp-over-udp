1e023e3b5b4124b36484fde2a0105c61361183f9
Kandida Gunawangit config --global user.name Kandida <kandidagunawan@gmail.com>
2023-11-25 05:47:52 +0700

    add: tests, make segment sequence num signed

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 # Fix pake ini sudo sysctl -w net.inet.udp.maxdgram=65535
 # Kalo komputer di reboot ga bs lagi, jadi harus di run lagi
 
+Cara pake:
+nyalain server
+ sudo python3 src/server.py 123 test/18mb.jpg
+
+nyalain klien
+ sudo python src/client.py 100 123 tesmee
+
 Progress:
 1. Segment (Done)
 2. Connection (Done)
-3. Three Way Handshake
-4. File Transfer
+3. Three Way Handshake (OK)
+4. File Transfer (Edge cases not done, simulated on reliable network only)
--- src/client.py
+++ src/client.py
@@ -3,7 +3,6 @@ import os
 import time
 from lib.connection import Connection, MessageInfo, Segment, SegmentFlag
 
-
 class Client:
     def __init__(self, client_port, broadcast_port, output_path):
         self.client_port = int(client_port)
@@ -12,20 +11,17 @@ class Client:
         self.connection = Connection(ip="localhost", port=self.client_port)
 
     def initiate_handshake(self):
-        syn_segment = Segment.syn(0)
+        syn_segment = Segment.syn(0) 
         self.connection.send("localhost", self.broadcast_port, syn_segment)
-        print("[!] [Handshake] Sending broadcast SYN request to port",
-              self.broadcast_port)
+        print("[!] [Handshake] Sending broadcast SYN request to port", self.broadcast_port)
 
         try:
-            message_info = self.connection.listen()
+            message_info = self.connection.listen(timeout=300)
             received_segment = message_info.segment
             if received_segment.is_syn() and received_segment.is_ack() and received_segment.is_valid_checksum():
-                print(
-                    "[!] [Handshake] Received valid SYN-ACK response. Initiating ACK...")
+                print("[!] [Handshake] Received valid SYN-ACK response. Initiating ACK...")
                 ack_segment = Segment.ack(0, 0)
-                self.connection.send(
-                    message_info.ip, message_info.port, ack_segment)
+                self.connection.send(message_info.ip, message_info.port, ack_segment)
                 print("[!] [Handshake] Handshake complete.")
             else:
                 print("[!] [Handshake] Received invalid SYN-ACK response. Exiting...")
@@ -36,49 +32,43 @@ class Client:
 
     def receive_data(self):
         expected_sequence_number = 0
-        received_data = b""
+        received_data = b"" 
 
         while True:
             try:
                 message_info = self.connection.listen(timeout=10)
                 received_segment = message_info.segment
 
+                if not received_segment.is_valid_checksum():
+                    print("[!] Invalid checksum received. Retrying...")
+                    continue
+
                 if received_segment.is_fin():
-                    print("[Segment SEQ={}] Received FIN. Acking and closing connection.".format(
-                        received_segment.sequence_number))
-                    ack_segment = Segment.ack(
-                        expected_sequence_number, received_segment.sequence_number)
-                    self.connection.send(
-                        message_info.ip, message_info.port, ack_segment)
+                    print("[Segment SEQ={}] Received FIN. Acking and closing connection.".format(received_segment.sequence_number))
+                    ack_segment = Segment.ack(expected_sequence_number, received_segment.sequence_number)
+                    self.connection.send(message_info.ip, message_info.port, ack_segment)
                     break
-                elif received_segment.sequence_number == expected_sequence_number and received_segment.is_valid_checksum():
-                    print("[Segment SEQ={}] Received, Ack sent".format(
-                        received_segment.sequence_number))
+                elif received_segment.sequence_number == expected_sequence_number:
+                    print("[Segment SEQ={}] Received, Ack sent".format(received_segment.sequence_number))
 
                     received_data += received_segment.data_payload
 
-                    ack_segment = Segment.ack(
-                        expected_sequence_number, received_segment.sequence_number)
-                    self.connection.send(
-                        message_info.ip, message_info.port, ack_segment)
+                    ack_segment = Segment.ack(expected_sequence_number, received_segment.sequence_number)
+                    self.connection.send(message_info.ip, message_info.port, ack_segment)
 
                     expected_sequence_number += 1
                 else:
-                    raise Exception("Invalid segment received.")
-
+                    print("[Segment SEQ={}] Ignoring, invalid segment received. Expected SEQ={}".format(received_segment.sequence_number, expected_sequence_number))
+                    
             except Exception as e:
-                print(e)
-                print("[!] Retrying...")
-                print("[Segment SEQ={}] Something went wrong.. Ack previous sequence number.".format(
-                    expected_sequence_number))
-                ack_segment = Segment.ack(
-                    expected_sequence_number, expected_sequence_number)
-                self.connection.send(
-                    message_info.ip, message_info.port, ack_segment)
+                print("[!] Timeout waiting for segment. Retrying...")
+                ack_segment = Segment.ack(expected_sequence_number-1, expected_sequence_number-1)
+                self.connection.send(message_info.ip, message_info.port, ack_segment)
 
-        output_filename = self.output_path
+        output_filename=self.output_path
         with open(output_filename, 'wb') as output_file:
             output_file.write(received_data)
+
 
     def start(self):
         print(f"[!] Client started at localhost:{self.client_port}")
@@ -92,8 +82,7 @@ class Client:
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print(
-            "Usage: python3 client.py [client port] [broadcast port] [path output]")
+        print("Usage: python3 client.py [client port] [broadcast port] [path output]")
         print("sudo python3 server.py 123 client.py")
         sys.exit(1)
 
