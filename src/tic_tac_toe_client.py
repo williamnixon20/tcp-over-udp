f0e7e6628888da39f0a127dd0ba2e1e567adb2a1
debbyalmadea <almadeaputri@gmail.com>
2023-11-25 21:07:50 +0700

    add: tic tac toe game, server, and client

---
+++ src/tic_tac_toe_client.py
@@ -0,0 +1,71 @@
+from node import Node
+
+import sys
+import json
+
+
+class TicTacToeClient(Node):
+    def __init__(self, client_port, server_ip, server_port):
+        super().__init__(client_port)
+        self.server_port = int(server_port)
+        self.server_ip = server_ip
+        self.server_address = (self.server_ip, self.server_port)
+
+    def start(self):
+        self.initiate_handshake(self.server_port)
+        while True:
+            self.receive_state()
+
+    def receive(self, timeout=0.1):
+        data = json.loads(super().receive(timeout).decode("utf-8"))
+        print(
+            f"[!] Received data from {self.server_address}")
+        return data
+
+    def receive_state(self):
+        data = self.receive(30)
+        print(f"\nMessage: {data['message']}")
+        print(f"Board:\n")
+        print(data["board"])
+
+        if data.get("is_game_over"):
+            self.connection.close()
+            sys.exit(0)
+
+        print(f"Current player: {data['current_player_symbol']}")
+        print(f"Is current player: {data['is_current_player']}\n")
+
+        if data['is_current_player']:
+            self.send_move()
+        else:
+            print(f"Waiting for other player to move...\n")
+
+    def receive_message(self):
+        data: dict = self.receive(10)
+        print(f"\nMessage:")
+        print(data["message"])
+
+        self.receive_state()
+        if data.get("is_winner") is not None or data.get("is_tie") is not None:
+            self.connection.close()
+            sys.exit(0)
+
+    def send_move(self):
+        print(f"[!] Enter move in format 'row col' e.g. 1 2.")
+        move = input("[?] Enter move (30s): ")
+        print()
+
+        self.send(self.server_address, move.encode('utf-8'))
+        print(f"[!] Sent move {move} to {self.server_address}.")
+        self.receive_state()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(
+            "Usage: python3 tic_tac_toe_client.py [client port] [server ip] [server port]")
+        sys.exit(1)
+
+    client_port, server_ip, server_port = sys.argv[1], sys.argv[2], sys.argv[3]
+    client = TicTacToeClient(client_port, server_ip, server_port)
+    client.start()
---
+++ src/tic_tac_toe_server.py
@@ -0,0 +1,147 @@
+from node import Node
+from game.tic_tac_toe import TicTacToe, Player
+import sys
+import json
+
+
+class TicTacToeServer(Node):
+    def __init__(self, server_port):
+        super().__init__(server_port)
+        self.tic_tac_toe = TicTacToe()
+        self.players = []
+
+    def listen_for_players(self):
+        print("[!] Listening to broadcast address for players.")
+        while len(self.players) < 2:
+            try:
+                message_info = self.connection.listen()
+                player_address = (message_info.ip, message_info.port)
+
+                if player_address not in self.players:
+                    self.players.append(player_address)
+                    print(f"[!] Received request from {player_address}")
+                    print(f"[!] Listening for more players..")
+
+            except Exception as e:
+                print("[!] Error while listening for players:", e)
+
+        print("[!] All players connected.")
+        print("\nPlayer list:")
+        for i, player in enumerate(self.players, start=1):
+            print(f"{i}. {player[0]}:{player[1]}")
+
+    def receive_move(self):
+        print()
+        received_data = self.receive(30)
+        print()
+        move = received_data.decode("utf-8")
+        return move
+
+    def send_message(self, player_address, message: dict):
+        print(f"[!] Sending message to {player_address}: {message}")
+        self.send(player_address, json.dumps(message).encode("utf-8"))
+
+    def send_state(self, player, message: str = ""):
+        print()
+        current_player = self.tic_tac_toe.get_current_player()
+        print(
+            f"[!] Sending state to {player.symbol} with address {player.ip}:{player.port}")
+        dict = self.tic_tac_toe.to_dict(player == current_player)
+        dict["message"] = message
+        self.send(
+            player.address,
+            json.dumps(dict).encode("utf-8"),
+        )
+        print()
+
+    def start_game(self):
+        player1 = Player("X", self.players[0][0], self.players[0][1])
+        player2 = Player("O", self.players[1][0], self.players[1][1])
+
+        self.tic_tac_toe.add_player(player1)
+        self.tic_tac_toe.add_player(player2)
+
+        print("\n=========== Game started ===========\n")
+        for player in self.tic_tac_toe.players:
+            self.send_state(
+                player, "Game started. Your symbol: " + player.symbol)
+        while True:
+            current_player: Player = self.tic_tac_toe.get_current_player()
+            print(f"Current player: {current_player.symbol}")
+            print("Current board:\n")
+            self.tic_tac_toe.print_board()
+
+            print("\nWaiting for move...")
+            move = self.receive_move()
+            print(f"Received move: {move}")
+
+            try:
+                row, col = move.split(" ")
+                row, col = int(row), int(col)
+
+                if self.tic_tac_toe.move(current_player, row, col):
+                    print("> Move successful.")
+                else:
+                    print("> Move unsuccessful.")
+                    self.send_state(
+                        current_player, "Invalid move. Please try again.")
+                    continue
+
+                if self.tic_tac_toe.check_winner(current_player.symbol):
+                    self.tic_tac_toe.print_board()
+                    print(f"> Player {current_player.symbol} wins!")
+                    self.send_state(
+                        current_player, "Congratulations! You win!")
+                    self.tic_tac_toe.switch_player()
+                    self.send_state(
+                        self.tic_tac_toe.get_current_player(
+                        ), f"You Lose. Player {current_player.symbol} wins."
+                    )
+                    break
+
+                if self.tic_tac_toe.is_board_full():
+                    self.tic_tac_toe.print_board()
+                    print(f"> Game is a tie!")
+                    self.send_state(
+                        current_player, "Game is a tie!")
+                    self.tic_tac_toe.switch_player()
+                    self.send_state(
+                        self.tic_tac_toe.get_current_player(
+                        ), "Game is a tie!"
+                    )
+                    break
+
+                self.tic_tac_toe.switch_player()
+                self.send_state(
+                    current_player, "Move successful.")
+                self.send_state(
+                    self.tic_tac_toe.get_current_player(), "Your turn.")
+            except Exception as e:
+                print("[!] Error while processing move:", e)
+
+        self.connection.close()
+        sys.exit(0)
+
+    def start(self):
+        print(f"[!] Server started at localhost:{self.node_port}")
+        self.listen_for_players()
+
+        for player_address in self.players:
+            if self.respond_handshake(player_address):
+                print(
+                    f"[!] [Player {player_address}] Handshake complete."
+                )
+
+        self.start_game()
+        self.connection.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 server.py [server port]")
+        print("sudo python3 src/tic_tac_toe_server.py 243")
+        sys.exit(1)
+
+    server_port = sys.argv[1]
+    server = TicTacToeServer(server_port)
+    server.start()
